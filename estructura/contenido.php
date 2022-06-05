ecf7d356de92188e30e400f319de47088e50ca8f
Jesus-ASG <jesus.serrano.graciano@gmail.com>
2022-06-05 00:46:56 -0500

    notas funcionando

--- estructura/contenido.php
+++ estructura/contenido.php
@@ -1,34 +1,31 @@
 <div class="container">
 	<div class="principal">
-		<h1>Bienvenido!</h1>
+		<h1>¡Te damos la bienvenida!</h1>
 		
 		<hr>
 		<div class="row">
 			<div class="col-6">
 				<h2>Notas</h2>
-				<div id="nueva_nota">
-					s
-				</div>
 				<div class="agregar_nota mx-3 mt-2" id="agregar_nota">
 					+ Agregar nota
+				</div>
+				<div id="nueva_nota">
+					
 				</div>
 			</div>
 
 			<div class="col-6">
 				<h2>Agenda</h2>
+				<div class="mx-3 mt-2" id="agregar_nota">
+					<h3>-- Próximamente --</h3>
+				</div>
 			</div>
 		</div>
 		
 		<br>
 		<br>
 		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
-		<br>
+		<hr>
 
 
 
--- js/script-index.js
+++ js/script-index.js
@@ -3,12 +3,33 @@ const agregar_nota = document.querySelector("#agregar_nota");
 const nueva_nota = document.querySelector("#nueva_nota");
 
 
-
+var num_notas = 0;
+let arr = [];
 agregar_nota.addEventListener("click", function(e){
-	console.log("click");
-	const di = document.createElement("input");
-	di.type = "text";
-	di.className = "nueva_nota";
-	di.textContent = "Hola soy un nuevo div";
-	nueva_nota.appendChild(di);
+	const div = document.createElement("div");
+	div.className = "nueva_nota mx-3 mt-2";
+
+	const inp = document.createElement("textarea");
+
+	var id_nota = "nota"+num_notas;
+	num_notas++;
+	inp.setAttribute("id", id_nota);
+
+	div.appendChild(inp);
+	nueva_nota.appendChild(div);
+	//nueva_nota.append(div);
+	inp.focus();
+
+	agregar(id_nota);
 });
+
+function agregar(id_nota){
+	const textarea = document.querySelector("#"+id_nota);
+
+	textarea.addEventListener("keyup", e =>{
+        textarea.style.height = "60px";
+        let scHeight = e.target.scrollHeight;
+        textarea.style.height = `${scHeight}px`;
+    });
+    arr.push(textarea);
+}
