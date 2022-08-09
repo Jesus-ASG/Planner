bf1b64f53b7f3bb59f80ffec9bf931ab2a90b26a
Jesus-ASG <jesus.serrano.graciano@gmail.com>
2022-08-08 23:51:20 -0500

    starting jwt for save sessions

--- public/index.js
+++ public/index.js
@@ -1,35 +1,47 @@
 const form_login = document.getElementById('form_login');
+let received_token;
+/////////////////////
+fetch('http://localhost:5000/login', {
+    headers: {
+        'Authorization': 'Bearer'
+    },
+    method: 'GET'
+})
+    .then(response => received_token)
+    .then(data => console.log('message received'));
+////////////////////
+
 
 form_login.addEventListener("submit", (e) => {
     e.preventDefault(); // prevents reload page when form is submited
-    
+
     let username = form_login.elements['username'].value; //getting form data
     let password = form_login.elements['password'].value;
 
     fetch('http://localhost:5000/login', {  // fetch login [0]
-        headers : {
-            'Content-type' : 'application/json' // indicate to server data type
+        headers: {
+            'Content-type': 'application/json' // indicate to server data type
         },
-        method : 'POST',    // indicate to server method type
-        body : JSON.stringify({username : username, password : password})   // write data
+        method: 'POST',    // indicate to server method type
+        body: JSON.stringify({ username: username, password: password })   // write data
     })
-    .then(response => response.json())  // send json to app.js in url/login -POST- [1]
-    .then(data => loadUserData(data['data'])); // recieves data that app.js sent after get results from database in url/login -POST-, and passes it to a function for do things with it [3] -> [4]
+        .then(response => response.json())  // send json to app.js in url/login -POST- [1]
+        .then(data => loadUserData(data['data'])); // recieves data that app.js sent after get results from database in url/login -POST-, and passes it to a function for do things with it [3] -> [4]
 });
 
-function loadUserData(data){    // [4]
-    if (data.length === 0){
+function loadUserData(data) {    // [4]
+    if (data.length === 0) {
         const form_message = document.getElementById('form_message');
         form_message.className = 'alert alert-warning';
         form_message.innerHTML = 'Los datos no coinciden';
     }
-    else{
+    else {
         // remove all unnecessary elements
         const btn_contact = document.getElementById('btn_contact');
         const btn_view_profile = document.getElementById('btn_view_profile');
         const btn_sign_out = document.getElementById('btn_sign_out');
         const access_container = document.getElementById('access_container');
-        
+
         btn_contact.style.display = 'none';
         btn_view_profile.hidden = false;
         btn_sign_out.hidden = false;
@@ -38,7 +50,7 @@ function loadUserData(data){    // [4]
         // place neccessary elements
         const main_container = document.getElementById('main_container');
         main_container.hidden = false;
-        
-        
+
+
     }
 }
--- server/app.js
+++ server/app.js
@@ -3,9 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded( { extended : false } );
 
+const jwt = require('jsonwebtoken')
+
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const dbService = require('./dbService');
 const { response } = require('express');
@@ -18,19 +19,37 @@ app.use(express.static('public'));
 // ROUTES
 
 // Login
-app.get('/login', (request, response) => {
+app.get('/login', authenticateToken, (request, response) => {
     console.log('hi from get method');
 });
 app.post('/login', (request, response) => {
     const { username, password } = request.body; // recieve what data index.js sent [1]->[2]
     const db = dbService.getDbServiceInstance(); // get reference from database
     const result = db.verifyLogin(username, password);  // call function in database, result is the output
+    
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
 
     result
-    .then(data=> response.json({data : data}))  // send database output to index.js [3]
+    .then(data => response.json({data : data})
+        )  // send database output to index.js [3]
     .catch(err => console.log(err));    // catch an error if it exists
+
+    
+
+
 });
 
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['autorization']
+    const token = authHeader && authHeader.split(' ')[1] // if exist return token otherwise return undefined
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) =>{
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(process.env.PORT, () => {
     console.log(`app running at http://localhost:${process.env.PORT}`);
